8b5a8902855f06ede673cd855c87c669e27c09bb
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-05-31 14:44:25 +0300

    Using several components. Components combining.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import { createRoot } from "react-dom/client";
 
-const el = (
-	<div>
-		<h1>Works</h1>
-		<p>Okay</p>
-	</div>
-);
+const SearchPanel = () => {
+	return (
+		<input type="text" />
+	);
+};
+
+const Todoitem = () => {
+	return (
+		<li>Item</li>
+	);
+};
+
+const TodoList = () => {
+	return (<ul>
+		<Todoitem />
+	</ul>);
+};
+
+const App = () => {
+	return (
+		<div id="app">
+			<SearchPanel />
+			<TodoList />
+		</div>
+	);
+};
 
 createRoot(
 	document.querySelector('#root'))
 .render(
-	el
+	<App />
 );
