a0682cc7314d1b99cd6074a600ad7812f91a6c03
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-05-31 14:34:08 +0300

    Basic React component manual rendering.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Works');
+import { createRoot } from "react-dom/client";
+
+const el = (
+	<div>
+		<h1>Works</h1>
+		<p>Okay</p>
+	</div>
+);
+
+createRoot(
+	document.querySelector('#root'))
+.render(
+	el
+);
