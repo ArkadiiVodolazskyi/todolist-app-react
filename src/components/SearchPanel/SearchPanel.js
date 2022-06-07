cbd69e230fd135ab12d3ad4c68a39bbdd39da529
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-06-07 14:17:48 +0300

    Refactored two functional based components into class based ones.

--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -1,14 +1,16 @@
-const SearchPanel = () => {
-	return (
-		<div className="SearchPanel d-flex justify-content-between align-items-center py-2">
-			<input type="text" />
-			<div className="btn-group btn-group-md">
-				<button type="button" className="btn btn-primary">All</button>
-				<button type="button" className="btn btn-outline-primary">Active</button>
-				<button type="button" className="btn btn-outline-primary">Done</button>
-			</div>
-		</div>
-	);
-};
+import React from 'react';
 
-export default SearchPanel;
+export default class SearchPanel extends React.Component {
+	render() {
+		return (
+			<div className="SearchPanel d-flex justify-content-between align-items-center py-2">
+				<input type="text" />
+				<div className="btn-group btn-group-md">
+					<button type="button" className="btn btn-primary">All</button>
+					<button type="button" className="btn btn-outline-primary">Active</button>
+					<button type="button" className="btn btn-outline-primary">Done</button>
+				</div>
+			</div>
+		);
+	}
+}
--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,27 +1,33 @@
+import React from 'react';
 import './TodoItem.css';
 
-const TodoItem = ({
-	label,
-	isImportant = false
-}) => {
-	return (
-		<li
-			className={
-				"TodoItem list-group-item d-flex justify-content-between align-items-center px-1 py-2 "
-				+ (isImportant ? 'important' : '' )
-			}
-		>
-			<span className="px-2">{ label }</span>
-			<div>
-				<button type="button" className="check_delete btn btn-sm btn-danger mx-1">
-					<i className="fa-solid fa-trash"></i>
-				</button>
-				<button type="button" className="check_done btn btn-sm btn-success mx-1">
-					<i className="fa-solid fa-check"></i>
-				</button>
-			</div>
-		</li>
-	);
-};
+export default class TodoItem extends React.Component {
+	render() {
+		const {
+			label,
+			isImportant = false
+		} = this.props;
 
-export default TodoItem;
+		return (
+			<li
+				className={
+					"TodoItem list-group-item d-flex justify-content-between align-items-center px-1 py-2 "
+					+ (isImportant ? 'important' : '' )
+				}
+			>
+				<span className="px-2">{ label }</span>
+				<div>
+					<button type="button" className="check_delete btn btn-sm btn-danger mx-1">
+						<i className="fa-solid fa-trash"></i>
+					</button>
+					<button type="button" className="check_important btn btn-sm btn-info mx-1 text-light">
+						<i class="fa-solid fa-exclamation"></i>
+					</button>
+					<button type="button" className="check_done btn btn-sm btn-success mx-1">
+						<i className="fa-solid fa-check"></i>
+					</button>
+				</div>
+			</li>
+		);
+	}
+}
