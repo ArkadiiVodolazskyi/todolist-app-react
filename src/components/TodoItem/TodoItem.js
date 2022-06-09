a59ba92493a9c2b3ef238a3b035d01a0d23a574e
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-06-09 23:29:13 +0200

    Working with state (two variants) and setState (two variants).

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -4,47 +4,72 @@ import './TodoItem.css';
 export default class TodoItem extends React.Component {
 
 	// Variant 1 - modern
-	process_item_lick = () => {
-		console.log(`Clicked: ${this.props.label}`)
+	state = {
+		isImportant: this.props.isImportant || false,
+		isDone: this.props.isDone || false
 	}
 
 	// Variant 2 - classic
 	// constructor() {
 	// 	super();
-
-	// 	this.process_item_lick = () => {
-	// 		console.log(`Clicked: ${this.props.label}`)
+	// 	this.state = {
+	// 		isDone: false
 	// 	}
 	// }
 
-	render() {
-		const {
-			label,
-			isImportant = false
-		} = this.props;
+	process_item_click = () => {
+		// Variant 1 - optimized
 
-		// Variant 3 - mine
-		// ! should be used without 'this'
-		// const process_item_lick = () => {
-		// 	console.log(`Clicked: ${this.props.label}`)
-		// }
+		// this.setState((state) => {
+		// 	return {
+		// 		isDone: !this.state.isDone
+		// 	};
+		// });
+
+		// Variant 1.1 - more optimized
+		this.setState(({isDone}) => {
+			return {
+				isDone: !this.state.isDone
+			};
+		});
+
+		// Variant 2 - classic
+		// this.setState({
+		// 	isDone: !this.state.isDone
+		// });
+	}
+
+	change_importance = () => {
+		this.setState({
+			isImportant: !this.state.isImportant
+		});
+	}
+
+	render() {
+		const { label } = this.props;
+		const { isImportant, isDone } = this.state;
 
 		return (
 			<li
 				className={
 					"TodoItem list-group-item d-flex justify-content-between align-items-center p-0"
 					+ (isImportant ? ' important' : '' )
+					+ (isDone ? ' done' : '' )
 				}
 			>
 				<span
 					className="TodoItem_label d-inline-flex flex-grow-1 p-2"
-					onClick={this.process_item_lick}
+					onClick={this.process_item_click}
 				>{ label }</span>
 				<div className='TodoItem_buttons p-2'>
 					<button type="button" className="check_delete btn btn-sm btn-danger mx-1">
 						<i className="fa-solid fa-trash"></i>
 					</button>
-					<button type="button" className="check_important btn btn-sm btn-info mx-1 text-light">
+					<button
+						type="button"
+						className="check_important btn btn-sm btn-info mx-1 text-light"
+						onClick={this.change_importance}
+					>
 						<i className="fa-solid fa-exclamation"></i>
 					</button>
 					<button type="button" className="check_done btn btn-sm btn-success mx-1">
