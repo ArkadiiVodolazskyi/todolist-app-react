7a8b6a0c96f9f26b168a6fec880f555b4bb58474
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-06-09 22:55:32 +0200

    Events handling: three variants.

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -2,26 +2,50 @@ import React from 'react';
 import './TodoItem.css';
 
 export default class TodoItem extends React.Component {
+
+	// Variant 1 - modern
+	process_item_lick = () => {
+		console.log(`Clicked: ${this.props.label}`)
+	}
+
+	// Variant 2 - classic
+	// constructor() {
+	// 	super();
+
+	// 	this.process_item_lick = () => {
+	// 		console.log(`Clicked: ${this.props.label}`)
+	// 	}
+	// }
+
 	render() {
 		const {
 			label,
 			isImportant = false
 		} = this.props;
 
+		// Variant 3 - mine
+		// ! should be used without 'this'
+		// const process_item_lick = () => {
+		// 	console.log(`Clicked: ${this.props.label}`)
+		// }
+
 		return (
 			<li
 				className={
-					"TodoItem list-group-item d-flex justify-content-between align-items-center px-1 py-2 "
-					+ (isImportant ? 'important' : '' )
+					"TodoItem list-group-item d-flex justify-content-between align-items-center p-0"
+					+ (isImportant ? ' important' : '' )
 				}
 			>
-				<span className="px-2">{ label }</span>
-				<div>
+				<span
+					className="TodoItem_label d-inline-flex flex-grow-1 p-2"
+					onClick={this.process_item_lick}
+				>{ label }</span>
+				<div className='TodoItem_buttons p-2'>
 					<button type="button" className="check_delete btn btn-sm btn-danger mx-1">
 						<i className="fa-solid fa-trash"></i>
 					</button>
 					<button type="button" className="check_important btn btn-sm btn-info mx-1 text-light">
-						<i class="fa-solid fa-exclamation"></i>
+						<i className="fa-solid fa-exclamation"></i>
 					</button>
 					<button type="button" className="check_done btn btn-sm btn-success mx-1">
 						<i className="fa-solid fa-check"></i>
