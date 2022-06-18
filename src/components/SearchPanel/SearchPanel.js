d3debad28019b42b43b21a771e2259aaa9b459bc
Arkadii Vodolazskyi <49953024+ArkadiiVodolazskyi@users.noreply.github.com>
2022-06-18 18:04:40 +0200

    Improvements on input for new todos.

--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './SearchPanel.css';
 
 export default class SearchPanel extends React.Component {
 
@@ -8,15 +9,16 @@ export default class SearchPanel extends React.Component {
 		input_value: ''
 	}
 
-	change_input_value = () => {
+	change_input_value = (e) => {
 		this.setState(() => {
 			return {
-				input_value: document.querySelector('.new_todo_title').value
+				input_value: e.target.value
 			};
 		});
 	}
 
-	handle_on_add = () => {
+	handle_on_add = (e) => {
+		e.preventDefault();
 		this.onAdd(this.state.input_value);
 		this.setState(() => {
 			return {
@@ -26,21 +28,21 @@ export default class SearchPanel extends React.Component {
 	}
 
 	render() {
-
-
 		return (
-			<div className="SearchPanel d-flex justify-content-between align-items-center py-2">
-				<input
-					type="text"
-					onChange={this.change_input_value}
-					className="new_todo_title px-2 py-1"
-					value={this.state.input_value}
-				/>
-				<button
-					type="button"
-					className="btn btn-success"
-					onClick={this.handle_on_add}
-				>Add</button>
+			<div className="SearchPanel d-flex justify-content-between align-items-center py-2 gap-3">
+				<form
+					className='input-group form_add_todo'
+					onSubmit={this.handle_on_add}
+				>
+					<input
+						type="text"
+						onChange={this.change_input_value}
+						className="form-control new_todo_title display-inline-block px-2 py-1"
+						value={this.state.input_value}
+						placeholder='New task'
+					/>
+					<input className="btn btn-success" type="submit" value="Add" />
+				</form>
 				<div className="btn-group btn-group-md">
 					<button type="button" className="btn btn-primary">All</button>
 					<button type="button" className="btn btn-outline-primary">Active</button>
